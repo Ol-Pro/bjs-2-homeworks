d4ee0610c8107566403a03f6df3825ada5b22a67
Олег Пронюшкин <oron@list.ru>
2023-06-11 19:12:41 +0500

    Задача-1 исправил

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = (b ** 2) - (4 * a * c);
-	if (d < 0) {
-		arr.push();
-	} else if (d == 0) {		
+	if (d == 0) {		
 		let oneRoot = -b / (2 * a);
 			arr.push(oneRoot);
 	} else if (d > 0) {
