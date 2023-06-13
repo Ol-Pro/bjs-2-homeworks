a0f7a8e182e59acbf30ad84f36b7cc070521c476
Олег Пронюшкин <oron@list.ru>
2023-06-13 15:25:06 +0500

    Юзер

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+    let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+    return result; // boolean
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(el => el.gender === gender).reduce((accum, averageAge, index, arrayLength) => accum + averageAge.age / arrayLength.length, 0);
+    return result 
 }
